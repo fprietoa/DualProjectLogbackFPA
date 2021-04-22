a9459b21c80b631a9e04bbe34a3a896698fccd5f
fprietoa <fprietoa@everis.com>
2021-04-22 14:10:07 +0200

    Proyecto Logback v2

--- src/main/java/com/everis/logback_desafio_project/App.java
+++ src/main/java/com/everis/logback_desafio_project/App.java
@@ -22,7 +22,10 @@ public class App {
 	 * @param args
 	 */
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+		
+		LOGGER.info("Proyecto de Logback");
+		
 		LOGGER.info("Inicio del método");
 		
 		// Iteraciones hasta N.
@@ -31,6 +34,22 @@ public class App {
 		}
 		
 		LOGGER.info("Fin del método");
+		
+		
+		LOGGER.info("Segundo bucle");
+		
+		LOGGER.info("Inicio del array");
+		
+		// Array con los niveles.
+		String[] logger = {"Trace", "Debug", "Info", "Warn", "Error"};
+		
+		// Iteraciones niveles Logger.
+		for(int i = 0; i<logger.length; i++) {
+			System.out.println("Niveles ordenados por importancia: " + logger[i]);		
+			LOGGER.debug("Iteración número: {}", i);
+		}
+		
+		LOGGER.info("Fin del array");
 
 	}
 }
